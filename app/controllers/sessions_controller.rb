f6b60eaf0fbcb3a43833e6135ec78e3961818c10
Roger Parent <pengytheduckwin@gmail.com>
2017-01-10 16:48:56 -0500

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      #Log the user in and redirect to the page they were at
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        #Log the user in and redirect to the page they were at
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message = "Account not activated. Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # Flash an error message
       flash.now[:danger] = 'Invalid email/password combination'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,7 +72,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user without a digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
   
 end
